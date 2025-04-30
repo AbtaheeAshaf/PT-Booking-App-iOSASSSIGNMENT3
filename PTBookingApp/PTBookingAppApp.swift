40631ed1572bbb748abbb4794d4d2b0c988cb0ef
AbtaheeAshaf <abtahee.ashaf@gmail.com>
2025-04-30 13:27:39 +1000

    Set up project workspace and structure for team collaboration

---
+++ PTBookingApp/PTBookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PTBookingAppApp.swift
+//  PTBookingApp
+//
+//  Created by Abtahee Ashaf on 30/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct PTBookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PTBookingAppTests/PTBookingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PTBookingAppTests.swift
+//  PTBookingAppTests
+//
+//  Created by Abtahee Ashaf on 30/4/2025.
+//
+
+import Testing
+@testable import PTBookingApp
+
+struct PTBookingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
